46ad0f6e767a64325afae04c14ad0bd8492fc234
Revenant <dasdev.pratik@gmail.com>
2021-12-17 11:54:02 +0530

    RES

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 
-const counterReducer = (state = 0,action) => {
+/* const counterReducer = (state = 0,action) => {
 switch(action.type){
   case 'INCREMENT':
     return state + 1
@@ -11,44 +11,58 @@ switch(action.type){
   case 'ZERO':
     return 0
     default:
+
+
     return state
   }
+} */
+const noteReducer = (state=[],action)=>{
+  if(action.type === 'NEW_NOTE'){
+    state.push(action.data)
+    return state
+  }
+  return state
 }
+const store = createStore(noteReducer)
 
-const store = createStore(counterReducer)
+store.dispatch({
+  type: 'NEW_NOTE',
+  data:{
+    content:'the app state is in redux store',
+    important:true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data:{
+    content:'state changes are made with backend',
+    important:false,
+    id: 2
+  }
+})
 
 const App = () => {
   return (
     <div>
-      <div>
-        {store.getState()}
-      </div>
-      <button
-      onClick={e => store.dispatch({type:'INCREMENT'})}
-      >
-        plus
-        </button>       
-
-        <button
-      onClick={e => store.dispatch({type:'DECREMENT'})}
-      >
-        minus
-        </button> 
-
-        <button
-      onClick={e => store.dispatch({type:'ZERO'})}
-      >
-        zero
-        </button> 
+       <ul>
+        {store.getState().map(note=><li key ={note.id}>
+          {note.content} <strong>{note.important ? 'important' : ''}</strong>
+        </li>
+        )}
+       </ul>
     </div>
   );
 }
+
 
 const renderApp = () => {
   ReactDOM.render(
     <App />,
   document.getElementById('root')
-)}
+                                                                                                                                                        
+  )}
 
 renderApp()
 store.subscribe(renderApp)
