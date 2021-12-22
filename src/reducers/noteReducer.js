f56dfbb8ab5caee92204feb7bbbf07e3fbd5d3ea
rev <dasdev.pratik@gmail.com>
2021-12-22 12:01:11 +0530

    6:array spread syntax

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,12 +1,37 @@
-import {createStore} from 'redux'
-
 const noteReducer = (state=[],action)=>{
-    if(action.type === 'NEW_NOTE'){
-      state.push(action.data)
-      return state
+    switch(action.type){
+      case 'NEW_NOTE':
+        // return state.concat(action.data)
+        return [...state, action.data]
+      case 'TOGGLE_IMPORTANCE':{
+        const id = action.data.id
+        const noteToChange = state.find(n => n.id === id)
+        const changedNote = {
+          ...noteToChange,
+          important: !noteToChange.important
+        }
+        return state.map(note => 
+          note.id !== id ? note : changedNote
+          )
     }
-    return state
+    default:
+      return state
   }
-  const store = createStore(noteReducer)
-module.export = noteReducer
+}
+
+module.exports = noteReducer
+
+
+/* Array Spread Method
+const number = [1,2,3]
+console,log([...number,4,5])
+// [1,2,3,4,5]
+console,log([number,4,5])
+// [[1,2,3],4,5]
+const numbers = [1,2,3,4,5,6]
+const [first,second....rest] = numbers
+console.log(first)//1
+console.log(second)//2
+console.log(rest)//[3,4,5,6]
+*/
     
