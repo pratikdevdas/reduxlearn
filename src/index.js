07addf4987512382e65574690fdc598e9515763e
rev <dasdev.pratik@gmail.com>
2022-01-07 13:39:13 +0530

    part 6.a of fullstack open completed

--- src/index.js
+++ src/index.js
@@ -1,86 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './App'
+import noteReducer from './reducers/noteReducer'
 
-const noteReducer = (state=[],action)=>{
-  if(action.type === 'NEW_NOTE'){
-    state.push(action.data)
-    return state
-  }
-  return state
-}
+// const noteReducer = (state=[],action)=>{
+//   if(action.type === 'NEW_NOTE'){
+//     state.push(action.data)
+//     return state
+//   }
+//   return state
+// }
+// const store = createStore(noteReducer)
+
+// store.dispatch({
+//   type: 'NEW_NOTE',
+//   data:{
+//     content:'the app state is in redux store',
+//     important:true,
+//     id: 1
+//   }
+// })
+
+// store.dispatch({
+//   type: 'NEW_NOTE',
+//   data:{
+//     content:'state changes are made with backend',
+//     important:false,
+//     id: 2
+//   }
+// })
+
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  data:{
-    content:'the app state is in redux store',
-    important:true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data:{
-    content:'state changes are made with backend',
-    important:false,
-    id: 2
-  }
-})
-
-const generateId = () => {
-  Math.floor(Math.random() * 1000000)
-}
-
-const App = () => {
-  const addNote = (event) =>{
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value =''
-
-    store.dispatch({
-      type: 'NEW_NOTE',
-      data: {
-        content,
-        importtant: false,
-        id: generateId()
-      }
-    })
-  } 
-
-  const toggleImportance = (id) => {
-    store.dispatch(
-      {
-        type: 'TOGGLE_IMPORTANCE',
-        data: {id}
-    }
-    )
-  }
-
-  return (
-    <div>
-      <form onSubmit={addNote}>
-        <input name="note" />
-        <button type='submit'>add</button>
-      </form>
-       <ul>
-      {/* mapping only note */}
-        {store.getState().map(note=><li key ={note.id}
-        onClick={()=> toggleImportance(note.id)}>
-          {note.content} <strong>{note.important ? 'important' : ''}</strong>
-        </li>
-        )}
-       </ul>
-    </div>
-  );
-}
-
-const renderApp = () => {
   ReactDOM.render(
-    <App />,
+    <Provider store = {store}>
+    <App />
+    </Provider>,
   document.getElementById('root')
   )
-}
-renderApp()
-store.subscribe(renderApp)
