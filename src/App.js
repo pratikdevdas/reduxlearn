a8c9fddd8eb5682b15027fe70f293fb7669cc649
rev <dasdev.pratik@gmail.com>
2022-01-08 16:18:33 +0530

    6.b done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import NewNote from './components/NewNote';
 import Notes from './components/Notes';
+import VisibilityFilter from './components/VisibilityFilter';
 
 const App = () => {
+
     return (
       <div>
-    <NewNote/>
-    <Notes/>   
+        <NewNote/>
+        <VisibilityFilter/>         
+        <Notes/>   
        </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,38 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore,combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import App from './App'
-import noteReducer from './reducers/noteReducer'
 
-// const noteReducer = (state=[],action)=>{
-//   if(action.type === 'NEW_NOTE'){
-//     state.push(action.data)
-//     return state
-//   }
-//   return state
-// }
-// const store = createStore(noteReducer)
+import noteReducer from './reducers/noteReducer';
+import filterReducer from './reducers/filterReducer';
 
-// store.dispatch({
-//   type: 'NEW_NOTE',
-//   data:{
-//     content:'the app state is in redux store',
-//     important:true,
-//     id: 1
-//   }
-// })
+import {createNote} from './reducers/noteReducer';
+import {filterChange} from './reducers/filterReducer';
 
-// store.dispatch({
-//   type: 'NEW_NOTE',
-//   data:{
-//     content:'state changes are made with backend',
-//     important:false,
-//     id: 2
-//   }
-// })
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
 
-const store = createStore(noteReducer)
+const store = createStore(reducer)
+
+store.subscribe(()=>console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combine reducers form one reducers from many simple reducers'))
 
   ReactDOM.render(
     <Provider store = {store}>
@@ -40,3 +27,34 @@ const store = createStore(noteReducer)
     </Provider>,
   document.getElementById('root')
   )
+
+
+
+
+  /* const noteReducer = (state=[],action)=>{
+  if(action.type === 'NEW_NOTE'){
+    state.push(action.data)
+    return state
+  }
+  return state
+}
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data:{
+    content:'the app state is in redux store',
+    important:true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data:{
+    content:'state changes are made with backend',
+    important:false,
+    id: 2
+  }
+})
+ */
